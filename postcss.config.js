3cec1ab99e61ffe62c9b5a20057caa8a44c41f6c
unleashit <unleashit@protonmail.com>
2024-02-05 16:39:01 -0800

    remove Vitest hack from postcss.config.js (causing issues with Next)

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 // after an upgrade, vite started throwing errors because plugins aren't required/imported in the config
 // but Next.js just wants the string name. Oddly, the problem only happens with the first plugin in the array
-const getPlugin = (plugin) =>
-  process.env.NODE_ENV === 'test' ? require(plugin) : plugin;
+// const getPlugin = (plugin) =>
+//   process.env.NODE_ENV === 'test' ? require(plugin) : plugin;
 
 let config = {
   plugins: [
-    getPlugin('postcss-flexbugs-fixes'),
+    'postcss-flexbugs-fixes',
     [
       'postcss-preset-env',
       {
